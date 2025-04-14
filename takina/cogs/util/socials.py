986ed82309a2073bf75ea335db015623829061dc
orangci <c@orangc.net>
2025-04-14 16:37:36 +0300

    feat(util.socials): add reddit command

--- takina/cogs/util/socials.py
+++ takina/cogs/util/socials.py
@@ -1,4 +1,4 @@
-import nextcord
+import nextcord, asyncpraw
 from nextcord.ext import commands
 from config import *
 from ..libs.oclib import *
@@ -34,6 +34,7 @@ class SocialsGitHub(commands.Cog):
         await interaction.send(embed=embed)
 
     async def fetch_user_information(self, username):
+        username = username[1:] if username.startswith("@") else username
         embed = nextcord.Embed(color=EMBED_COLOR, description="", title=username)
         github = Github(GITHUB_AUTH_TOKEN)
         try:
@@ -75,5 +76,79 @@ class SocialsGitHub(commands.Cog):
         # fmt: on
 
 
+class SocialsReddit(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command(
+        help="Social information command for Reddit. Usage: `reddit orangci`.",
+    )
+    async def reddit(self, ctx: commands.Context, *, username: str):
+        embed = await self.fetch_user_information(username)
+        await ctx.reply(embed=embed, mention_author=False)
+        pass
+
+    @nextcord.slash_command(
+        name="reddit", description="Social information command for Reddit."
+    )
+    async def slash_reddit(
+        self,
+        interaction: nextcord.Interaction,
+        *,
+        username: str = nextcord.SlashOption(
+            description="The Reddit username to fetch information on.",
+            required=True,
+        ),
+    ):
+        await interaction.response.defer()
+        embed = await self.fetch_user_information(username)
+        await interaction.send(embed=embed)
+
+    async def fetch_user_information(self, username):
+        username = username[3:] if username.startswith("/u/") else username
+        username = username[2:] if username.startswith("u/") else username
+
+        reddit = asyncpraw.Reddit(
+            client_id=REDDIT_CLIENT_ID,
+            client_secret=REDDIT_CLIENT_SECRET,
+            user_agent=BOT_NAME,
+        )
+
+        embed = nextcord.Embed(color=0xFF4500, description="")
+
+        try:
+            user = await reddit.redditor(username)
+            await user.load()
+        except:
+            embed.color = ERROR_COLOR
+            embed.description = ":x: That Reddit account does not exist."
+            return embed
+
+        # fmt: off
+        embed.set_thumbnail(url=user.icon_img)
+        embed.title = f"u/{user.name}"
+        embed.description += f"{user.subreddit.public_description}\n" if user.subreddit.public_description else ""
+        embed.description += f"\n**Joined Reddit**: <t:{int(user.created_utc)}:D>"
+        embed.description += f"\n**Post Karma**: {user.link_karma}"
+        embed.description += f"\n**Comment Karma**: {user.comment_karma}"
+        embed.description += f"\n**Total Posts**: {user.link_karma}"
+        embed.description += f"\n**Total Comments**: {user.comment_karma}"
+        moderated_subreddits = await user.moderated()
+        moderated_subreddit_names = [f"r/{sub.display_name}" for sub in moderated_subreddits]
+        embed.description += f"\n**Moderator in**: {', '.join(moderated_subreddit_names)}" if moderated_subreddit_names else ""
+        return embed
+        # fmt: on
+
 def setup(bot: commands.Bot):
-    bot.add_cog(SocialsGitHub(bot))
+    if GITHUB_AUTH_TOKEN:
+        bot.add_cog(SocialsGitHub(bot))
+    else:
+        print(
+            "You must set the GITHUB_AUTH_TOKEN environment variable if you want the github command to work. Skipping loading the github command. To get your own github auth token, visit <https://github.com/settings/tokens/new>."
+        )
+    if REDDIT_CLIENT_ID and REDDIT_CLIENT_SECRET:
+        bot.add_cog(SocialsReddit(bot))
+    else:
+        print(
+            "You must set the REDDIT_CLIENT_ID and REDDIT_CLIENT_SECRET environment variables if you would like the reddit command to work. Skipping loading the reddit command. \nTo get your own Reddit application visit <https://www.reddit.com/prefs/apps>."
+        )
