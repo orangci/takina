3ffd1bc232a8e2eaf4d408c9ff7f72dc8fb077b2
orangc <c@orangc.net>
2026-06-06 17:40:06 +0300

    adfasdf

--- takina/cogs/settings.py
+++ takina/cogs/settings.py
@@ -1,5 +1,6 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 # SPDX-FileCopyrightText: orangc
+from pymongo import AsyncMongoClient
 from nextcord.ext import commands
 from ..libs import oclib
 import nextcord
