bf42c6d836228a1d9fd9f677fe0eae58529ab35b
orangc <c@orangc.net>
2026-07-21 16:43:28 +0300

    fix(util.qalc): fix the way the qalc cog is loaded and detects libqalculate

--- takina/cogs/util/qalc.py
+++ takina/cogs/util/qalc.py
@@ -8,52 +8,50 @@ import shutil
 import config
 import os
 
-qalc_package = os.getenv("QALC")
-
-
-async def qalc(expr: str):
-    proc = await asyncio.create_subprocess_exec(qalc_package, "-t", expr, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
-
-    try:
-        stdout, stderr = await proc.communicate()
-    except asyncio.CancelledError:
-        if proc.returncode is None:
-            proc.kill()
-        await proc.wait()
-        raise
-
-    if proc.returncode != 0:
-        return None
-
-    return stdout.decode().strip()
-
-
-async def qalc_embed(expr: str):
-    embed = nextcord.Embed(color=config.EMBED_COLOR)
-
-    try:
-        async with asyncio.timeout(config.QALC_TIMEOUT):
-            result = await qalc(expr)
-
-        if not result:
-            embed.description = ":x: Invalid expression."
-            embed.color = config.ERROR_COLOR
-        else:
-            embed.description = f"{await oclib.fetch_random_emoji()} {result}"
-    except TimeoutError:
-        embed.color = config.ERROR_COLOR
-        embed.description = ":x: Calculation timed out."
-
-    return embed
+qalc_package = os.getenv("QALC") or shutil.which("qalc")
 
 
 class Qalc(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
+    async def qalc_expression(self, expr: str):
+        proc = await asyncio.create_subprocess_exec(qalc_package, "-t", expr, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+
+        try:
+            stdout, stderr = await proc.communicate()
+        except asyncio.CancelledError:
+            if proc.returncode is None:
+                proc.kill()
+            await proc.wait()
+            raise
+
+        if proc.returncode != 0:
+            return None
+
+        return stdout.decode().strip()
+
+    async def qalc_embed(self, expr: str):
+        embed = nextcord.Embed(color=config.EMBED_COLOR)
+
+        try:
+            async with asyncio.timeout(config.QALC_TIMEOUT):
+                result = await self.qalc_expression(expr)
+
+            if not result:
+                embed.description = ":x: Invalid expression."
+                embed.color = config.ERROR_COLOR
+            else:
+                embed.description = f"{await oclib.fetch_random_emoji()} {result}"
+        except TimeoutError:
+            embed.color = config.ERROR_COLOR
+            embed.description = ":x: Calculation timed out."
+
+        return embed
+
     @commands.command(aliases=["calc", "calculate", "qalculate", "qc"], help="Mathematical calculation evaluation.", usage="6 * 7")
     async def qalc(self, ctx: commands.Context, *, calculation: str):
-        await ctx.reply(embed=await qalc_embed(calculation), mention_author=False)
+        await ctx.reply(embed=await self.qalc_embed(calculation), mention_author=False)
 
     @nextcord.slash_command(name="calculate", description="Mathematical calculation evaluation.")
     async def slash_qalc(
@@ -63,11 +61,11 @@ class Qalc(commands.Cog):
         calculation: str = nextcord.SlashOption(description="The mathematical calculation to evaluate.", required=True),
     ):
         await interaction.response.defer()
-        await interaction.send(embed=await qalc_embed(calculation), ephemeral=True)
+        await interaction.send(embed=await self.qalc_embed(calculation), ephemeral=True)
 
 
 def setup(bot: commands.Bot):
-    if shutil.which("qalc") is None and not config.NIXOS_INSTANCE:
+    if qalc_package is None and not config.NIXOS_INSTANCE:
         print("Skipping loading of util.qalc, as libqalculate is not installed/available on the system.")
         return
     bot.add_cog(Qalc(bot))
